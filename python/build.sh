ee59ce24cd9ba1935f03cd1d71e5f891e319b409
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-01-09 13:51:36 -0300

    update requirements

--- python/build.sh
+++ python/build.sh
@@ -1,3 +1,3 @@
-python manage.py collectstatic --no-input \ 
-    && python manage.py migrate \ 
+python manage.py collectstatic --no-input  
+    && python manage.py migrate  
     && gunicorn _core.wsgi --log-level debug
